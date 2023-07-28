a4952de40ae2caa3ab7bec02ab2f07b52951f203
Dheena <Dheena@DESKTOP-HPBU6KD>
2023-07-28 15:54:32 +0530

    commit

---
+++ C2TC/src/com/cg/dayone/Primitive.java
@@ -0,0 +1,11 @@
+package com.cg.dayone;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte range;
+			    range = 124;
+			    System.out.println(range);    // prints 124
+			  }
+			}
