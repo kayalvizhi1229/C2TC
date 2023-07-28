022a325915a61ecff36ae36a4fcba09c645b17bb
Dheena <Dheena@DESKTOP-HPBU6KD>
2023-07-28 20:23:28 +0530

    commit

---
+++ C2TC/src/com/cg/daytwo/Dowhile.java
@@ -0,0 +1,15 @@
+package com.cg.daytwo;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		do {
+		  System.out.println(i);
+		  i++;
+		}
+		while (i < 5);
+	}
+
+}
---
+++ C2TC/src/com/cg/daytwo/Foreach.java
@@ -0,0 +1,12 @@
+package com.cg.daytwo;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+	}
+}
