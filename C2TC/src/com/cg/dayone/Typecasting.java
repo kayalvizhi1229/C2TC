f5493be87e0842bbe71749f4474093c8d2682378
Dheena <Dheena@DESKTOP-HPBU6KD>
2023-07-28 20:03:20 +0530

    commit

---
+++ C2TC/src/com/cg/dayone/Typecasting.java
@@ -0,0 +1,12 @@
+package com.cg.dayone;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			     int num = 10;
+			    System.out.println("The integer value: " + num);
+			    double data = num;
+			    System.out.println("The double value: " + data);
+			  }
+			}
