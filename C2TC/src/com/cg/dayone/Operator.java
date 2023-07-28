00a6ae3bb6b7e24aff61878652eaee0e7cad1d2b
Dheena <Dheena@DESKTOP-HPBU6KD>
2023-07-28 15:42:48 +0530

    day one

---
+++ C2TC/src/com/cg/dayone/Operator.java
@@ -0,0 +1,14 @@
+package com.cg.dayone;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			    int a = 12, b = 5;		   
+			    System.out.println("a + b = " + (a + b));			    
+			    System.out.println("a - b = " + (a - b));			    
+			    System.out.println("a * b = " + (a * b));			   
+			    System.out.println("a / b = " + (a / b));			   
+			    System.out.println("a % b = " + (a % b));
+			  }
+			}
