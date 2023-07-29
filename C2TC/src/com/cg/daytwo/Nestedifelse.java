a789a32b7fa9798340c6ff82edf0b16ae131e66b
Dheena <Dheena@DESKTOP-HPBU6KD>
2023-07-30 00:26:09 +0530

    commit

---
+++ C2TC/src/com/cg/daytwo/Nestedifelse.java
@@ -0,0 +1,25 @@
+package com.cg.daytwo;
+
+public class Nestedifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n=24;
+        if (n % 2 == 0){
+            System.out.print("Even ");
+            if (n % 6 == 0) {
+                System.out.println("and divisible by 6");
+            } else {
+                System.out.println("and not divisible by 6");
+            }
+        } 
+else {
+            System.out.println("Odd ");
+            if(n % 3 == 0) {
+                System.out.println("and divisible by 3");
+            } else {
+                System.out.println("and not divisible by 3");
+            }
+        }
+	}
+}
